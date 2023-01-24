f0e7de60b2ccd9a43b4a828cc068b15d2dd246f1
Aleksandr Samoilov <sparco@nxt.ru>
2023-01-24 11:20:27 +0500

    first dz done

---
+++ pagination/stations/views.py
@@ -0,0 +1,33 @@
+
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.core.paginator import Paginator
+
+import csv
+
+
+def index(request):
+    return redirect(reverse('bus_stations'))
+
+
+def bus_stations(request):
+    with open('data-398-2018-08-30.csv', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        stations_list = []
+        for row in reader:
+            station = {'Name': row['Name'],
+                       'Street': row['Street'],
+                       'District': row['District']}
+            stations_list.append(station)
+
+    page_number = request.GET.get('page', 1)
+    paginator = Paginator(stations_list, 10)
+    page = paginator.get_page(page_number)
+    content = page.object_list
+    context = {
+        'bus_stations': content,
+        'page': page,
+
+
+    }
+    return render(request, 'stations/index.html', context)
